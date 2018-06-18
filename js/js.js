0beec00d52bffc2346b926b4d2d04e54f02df6e6
Neveh Allon <NevehAllon@gmail.com>
2018-06-18 03:55:35 +0300

    Work on Accessabilty

--- js/js.js
+++ js/js.js
@@ -1,19 +1,36 @@
 let mapped = $('#map-container');
 let list = $('.filter-options');
 let air = $('area');
+let print = $('.gmnoprint > img');
+let imgs = $('img');
 
-$(window).scroll(function() {
+$(window).scroll(() => {
     mapped.addClass("small");
     list.addClass("small-filter");
 });
 
 function setAttr() {
+    $('iframe')[0].title = "map";
+
     for (let i = 0; i < $('area').length; i++) {
-    $('area')[i].alt = $('area')[i].title;
+        $('area')[i].alt = $('area')[i].title;
+        $('.gmnoprint > img')[i].alt = "marker";
+    }
+
+
+    for (let j = 0; j <= $('img').length; j++) {
+        if (j <= 8) {
+            $('img')[j].alt = "marker";
+        } else if (j <= 21) {
+            $('img')[j].alt = "map";
+        } else if (j == 32) {
+            $('img')[j].alt = "google logo";
+        } else if (j > 32 && j <= 40) {
+            $('img')[j].alt = "accessibility icons";
+        }
     }
 }
 
 setTimeout(() => {
-    setAttr();    
+    setAttr();
 }, 1000);
-/* $('area')["0"].alt = $('area')["0"].title; */
