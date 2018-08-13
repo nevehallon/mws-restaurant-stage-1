84faf223e154af0a77a2b4b08e12aeb88d0fab1c
Neveh Allon <NevehAllon@gmail.com>
2018-08-13 16:06:41 +0300

    Working App

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
    /*  const port = 5500 */ // Change this to your server port
-    return `https://nevehallon.github.io/mws-restaurant-stage-1/`;
+    return `https://nevehallon.github.io/mws-restaurant-stage-1/data/restaurants.json`;
   }
 
   /**
